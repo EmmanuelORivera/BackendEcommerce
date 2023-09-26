09e7921de53bc62d1b4937eea62644e0a68b83bd
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-26 17:18:07 -0600

    initial commit

---
+++ src/Core/Ecommerce.Domain/ProductStatus.cs
@@ -0,0 +1,10 @@
+using System.Runtime.Serialization;
+
+namespace Ecommerce.Domain;
+
+public enum ProductStatus {
+    [EnumMember(Value ="Inactive Product")]
+    Inactive,
+    [EnumMember(Value ="Active Product")]
+    Active
+}
