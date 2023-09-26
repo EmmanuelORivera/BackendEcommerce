7acd50aa5d205f6488685581c01fe75cc95e8c6b
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-26 17:40:26 -0600

    refactor: autoformatting

--- src/Core/Ecommerce.Domain/Image.cs
+++ src/Core/Ecommerce.Domain/Image.cs
@@ -2,8 +2,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Ecommerce.Domain.Common;
 
 namespace Ecommerce.Domain;
-public class Image: BaseDomainModel{
-    [Column(TypeName ="NVARCHAR(4000)")]
+public class Image : BaseDomainModel
+{
+    [Column(TypeName = "NVARCHAR(4000)")]
     public string? Url { get; set; }
     public int ProductId { get; set; }
     public string? PublicCode { get; set; }
