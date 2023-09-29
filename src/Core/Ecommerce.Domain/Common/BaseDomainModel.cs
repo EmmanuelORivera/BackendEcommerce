3dc1eecdc53e295f45a39c9ea730725d8c54b777
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-28 23:45:51 -0600

    refactor: change type of LastModified to string

--- src/Core/Ecommerce.Domain/Common/BaseDomainModel.cs
+++ src/Core/Ecommerce.Domain/Common/BaseDomainModel.cs
@@ -1,9 +1,10 @@
 namespace Ecommerce.Domain.Common;
 
-public abstract class BaseDomainModel{
+public abstract class BaseDomainModel
+{
     public int Id { get; set; }
     public DateTime? CreatedDate { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime? LastModifiedDate { get; set; }
-    public DateTime? LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
