bbdd26ce2ea4a296750d83c0c0d84ee3de421e10
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-29 13:05:55 -0600

    feat: seeding initial data into an EF.

---
+++ src/Infrastructure/Persistence/EcommerceDbContextData.cs
@@ -0,0 +1,101 @@
+using Ecommerce.Application.Models.Authorization;
+using Ecommerce.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Ecommerce.Infrastructure.Persistence;
+
+public class EcommerceDbContextData
+{
+    public static async Task LoadDataAsync(
+        EcommerceDbContext context,
+        UserManager<User> userManager,
+        RoleManager<IdentityRole> roleManager,
+        ILoggerFactory loggerFactory)
+    {
+        try
+        {
+            if (!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(new IdentityRole(Role.ADMIN));
+                await roleManager.CreateAsync(new IdentityRole(Role.USER));
+            }
+
+            if (!userManager.Users.Any())
+            {
+                var adminUser = new User
+                {
+                    Name = "Emmanuel",
+                    LastName = "Ochoa",
+                    Email = "eo@test.com",
+                    UserName = "emmanuel.ochoa",
+                    Phone = "5555555555",
+                    AvatarUrl = "https://firebasestorage.googleapis.com/v0/b/edificacion-app.appspot.com/o/vaxidrez.jpg?alt=media&token=14a28860-d149-461e-9c25-9774d7ac1b24"
+                };
+
+                await userManager.CreateAsync(adminUser, "PasswordEmmanuelOchoa123");
+                await userManager.AddToRoleAsync(adminUser, Role.ADMIN);
+
+                var user = new User
+                {
+                    Name = "Juan",
+                    LastName = "Perez",
+                    Email = "jp@test.com",
+                    UserName = "juan.perez",
+                    Phone = "4444444444",
+                    AvatarUrl = "https://firebasestorage.googleapis.com/v0/b/edificacion-app.appspot.com/o/avatar-1.webp?alt=media&token=58da3007-ff21-494d-a85c-25ffa758ff6d"
+                };
+
+                await userManager.CreateAsync(adminUser, "PasswordJuanPerez123");
+                await userManager.AddToRoleAsync(adminUser, Role.USER);
+            }
+
+            if (!context.Categories!.Any())
+            {
+
+                var categoryData = File.ReadAllText("../Infrastructure/Data/category.json");
+                var categories = JsonConvert.DeserializeObject<List<Category>>(categoryData);
+                await context.Categories!.AddRangeAsync(categories!);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Products!.Any())
+            {
+                var productData = File.ReadAllText("../Infrastructure/Data/product.json");
+                var products = JsonConvert.DeserializeObject<List<Product>>(productData);
+                await context.Products!.AddRangeAsync(products!);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Images!.Any())
+            {
+                var imageData = File.ReadAllText("../Infrastructure/Data/image.json");
+                var images = JsonConvert.DeserializeObject<List<Image>>(imageData);
+                await context.Images!.AddRangeAsync(images!);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Reviews!.Any())
+            {
+                var reviewData = File.ReadAllText("../Infrastructure/Data/review.json");
+                var reviews = JsonConvert.DeserializeObject<List<Review>>(reviewData);
+                await context.Reviews!.AddRangeAsync(reviews!);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Countries!.Any())
+            {
+                var countriesData = File.ReadAllText("../Infrastructure/Data/countries.json");
+                var countries = JsonConvert.DeserializeObject<List<Country>>(countriesData);
+                await context.Countries!.AddRangeAsync(countries!);
+                await context.SaveChangesAsync();
+            }
+        }
+        catch (Exception e)
+        {
+            var logger = loggerFactory.CreateLogger<EcommerceDbContextData>();
+            logger.LogError(e.Message);
+        }
+    }
+}
