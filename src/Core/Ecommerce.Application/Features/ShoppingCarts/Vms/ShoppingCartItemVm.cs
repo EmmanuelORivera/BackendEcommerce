b8db28976d6f467eb3fd14ea19724719b69a4ed4
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-09 13:28:48 -0600

    feat: shopping cart VMs

---
+++ src/Core/Ecommerce.Application/Features/ShoppingCarts/Vms/ShoppingCartItemVm.cs
@@ -0,0 +1,23 @@
+namespace Ecommerce.Application.Features.ShoppingCarts.Vms;
+
+public class ShoppingCatsItemVm
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string? Product { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string? Category { get; set; }
+    public int Stock { get; set; }
+    public decimal TotalLine
+    {
+        get
+        {
+            return Math.Round(Quantity * Price, 2);
+        }
+        set
+        {
+
+        }
+    }
+}
---
+++ src/Core/Ecommerce.Application/Features/ShoppingCarts/Vms/ShoppingCartVm.cs
@@ -0,0 +1,43 @@
+namespace Ecommerce.Application.Features.ShoppingCarts.Vms;
+
+public class ShoppingCartVm
+{
+    public string? ShoppingCartId { get; set; }
+    public List<ShoppingCatsItemVm>? ShoppingCartItems { get; set; }
+    public decimal Total
+    {
+        get
+        {
+            return Math.Round(
+                ShoppingCartItems!.Sum(x => x.Price * x.Quantity) +
+                ShoppingCartItems!.Sum(x => x.Price * x.Quantity) * Convert.ToDecimal(0.18) +
+                ((ShoppingCartItems!.Sum(x => x.Price * x.Quantity)) < 100 ? 10 : 25)
+                , 2
+                );
+        }
+        set { }
+    }
+    public int Quantity
+    {
+        get { return ShoppingCartItems!.Sum(x => x.Quantity); }
+        set { }
+    }
+
+    public decimal SubTotal
+    {
+        get { return Math.Round(ShoppingCartItems!.Sum(x => x.Price * x.Quantity), 2); }
+    }
+    public decimal Tax
+    {
+        get { return Math.Round(((ShoppingCartItems!.Sum(x => x.Price * x.Quantity)) * Convert.ToDecimal(0.18)), 2); }
+        set { }
+    }
+    public decimal ShippingPrice
+    {
+        get
+        {
+            return (ShoppingCartItems!.Sum(x => x.Price * x.Quantity)) < 100 ? 10 : 25;
+        }
+        set { }
+    }
+}
