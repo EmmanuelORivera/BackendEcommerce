3b5552789645781ca42f8485b0a5ca6a3f98f6bd
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-15 13:26:01 -0600

    feat: specifications for the pagination on orders

---
+++ src/Core/Ecommerce.Application/Specifications/Orders/OrderForCountingSpecification.cs
@@ -0,0 +1,15 @@
+using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Orders;
+
+public class OrderForCountingSpecification : BaseSpecification<Order>
+{
+    public OrderForCountingSpecification(OrderSpecificationParams orderParams)
+    : base(
+        x =>
+        (string.IsNullOrEmpty(orderParams.Username) || x.BuyerUserName!.Contains(orderParams.Username)) &&
+        (!orderParams.Id.HasValue || x.Id == orderParams.Id)
+    )
+    {
+    }
+}
