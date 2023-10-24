6847d39c7bdfec9a8519e66903baf9f66c57f262
Emmanuel Ochoa <eorivera95@gmail.com>
2023-10-23 18:23:45 -0600

    fix: returning the description from the backend.

--- src/Core/Ecommerce.Application/Features/Products/Queries/Vms/ProductVm.cs
+++ src/Core/Ecommerce.Application/Features/Products/Queries/Vms/ProductVm.cs
@@ -10,6 +10,7 @@ public class ProductVm
     public int Id { get; set; }
     public string? Name { get; set; }
     public decimal Price { get; set; }
+    public string? Description { get; set; }
     public int Rating { get; set; }
     public string? Seller { get; set; }
     public int Stock { get; set; }
